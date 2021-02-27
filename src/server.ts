10a1b6889bd287d4453812b4a75333af7601569e
Davi <davi.nascimento777@gmail.com>
2021-02-27 11:25:59 -0300

    Basico de rota get e post

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,15 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
+
+
+app.get('/',(request,response)=>{
+    return response.json({msg:"status on"})
+})
+
+app.post('/',(request,response)=>{
+    return response.json({msg:"Dados salvos"})
+
+})
 
 app.listen(3333,()=>console.log("Server on!!!!"));

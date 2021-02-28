b912db4662091bad3c52521724ca8bfd53da627e
Davi <davi.nascimento777@gmail.com>
2021-02-28 11:06:28 -0300

    Criaçao basica de model para user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,7 +3,6 @@ import { Request, Response } from 'express'
 class UserController {
   async create(request: Request, response: Response) {
     const body = request.body;
-    console.log(body)
     return response.send()
 
   }

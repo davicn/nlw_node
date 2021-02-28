34338ba5b323d5a722cf0221a9db74c2efaca734
Davi <davi.nascimento777@gmail.com>
2021-02-28 10:34:35 -0300

    Criaçao de migration de user

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,13 @@ import express from 'express';
 const app = express();
 
 
-app.get('/',(request,response)=>{
-    return response.json({msg:"status on"})
+app.get('/', (request, response) => {
+  return response.json({ msg: "status on" })
 })
 
-app.post('/',(request,response)=>{
-    return response.json({msg:"Dados salvos"})
+app.post('/', (request, response) => {
+  return response.json({ msg: "Dados salvos" })
 
 })
 
-app.listen(3333,()=>console.log("Server on!!!!"));
+app.listen(3333, () => console.log("Server on!!!!"));

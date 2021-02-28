58e9cff1e045a903072dc63b8fb015351fa45454
Davi <davi.nascimento777@gmail.com>
2021-02-28 11:28:37 -0300

    Separando repository de user

---
+++ src/database/migrations/1614522477005-CreateSurveys.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateSurveys1614522477005 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
